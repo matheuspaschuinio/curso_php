26722086cd97c9ab44a714ea2cd50421fcedc19d
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-09-07 17:07:26 -0300

    Primeiro commit

---
+++ ex003/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+
+    <?php 
+        $num = 15;
+        echo "O valor da variável é $num"
+    ?>
+</body>
+</html>
